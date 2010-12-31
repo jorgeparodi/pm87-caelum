d30505ef030d72cf8f9791f65c3f263bba33d840
jorgeparodi <pamuvaz@uol.com.br>
2010-12-31 15:30:15 -0200

    aaaa

--- src/br/com/caelum/git/TesteGit.java
+++ src/br/com/caelum/git/TesteGit.java
@@ -3,7 +3,8 @@ package br.com.caelum.git;
 public class TesteGit {
 	
 	public static void main(String[] args) {
-		System.out.println("Exemplo de teste de uso do Git");
+		System.out.println("Exemplo de teste de uso do Git1");
+		System.out.println("Exemplo de teste de uso do Git2");
 	}
 
 }
